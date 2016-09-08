327517ea16b1689ce6cb93bc1e4a7a8fee2d9041
Joe Bruno <joevbruno@me.com>
2016-09-08 15:18:43 -0400

    updated models and decoder defs

--- src/Main.elm
+++ src/Main.elm
@@ -3,10 +3,8 @@ import Html.App as Html
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
-import Json.Decode exposing (Decoder)
+import Json.Decode as Json exposing (Decoder, decodeValue, succeed, string, oneOf, null, list, bool, (:=))
 import Task
-import Debug exposing (..)
-import Json.Decode.Pipeline exposing (decode, required, optional, hardcoded)
 
 main : Program Never
 main =
@@ -17,41 +15,135 @@ main =
         , view = view
         }
 
+type alias CompleteForecast = { latitude : Float
+                              , longitude : Float
+                              , timezone : String
+                              , currently : Forecast
+                              , hourly : TimespanForecast HourlyForecast
+                              , daily : TimespanForecast DailyForecast
+                              }
+
+
+type alias HourlyForecast = { time : Int
+                            , summary : String
+                            , icon : String
+                            , temperature : Float
+                            , precipIntensity : Float
+                            , precipProbability : Float
+                            }
+
+
+type alias TimespanForecast ft = { summary : String
+                                 , icon : String
+                                 , forecasts : List ft
+                                 }
+
+
+type alias Forecast = { time : Int
+                      , summary : String
+                      , icon : String
+                      , precipIntensity : Float
+                      , precipProbability : Float
+                      , temperature : Float
+                      , windSpeed : Float
+                      , windBearing : Float
+                      , humidity : Float
+                      , visibility : Float
+                      , cloudCover : Float
+                      , pressure : Float
+                      , ozone : Float
+                      }
+
+
+type alias DailyForecast = { time : Int
+                           , summary : String
+                           , icon : String
+                           , precipIntensity : Float
+                           , precipProbability : Float
+                           , temperatureMin : Float
+                           , temperatureMax : Float
+                           }
+
+
+timespanForecastDecoder : Json.Decoder ft -> Json.Decoder (TimespanForecast ft)
+timespanForecastDecoder dataDecoder =
+  Json.object3
+    TimespanForecast
+    ("summary" := Json.string)
+    ("icon" := Json.string)
+    ("data" := Json.list dataDecoder)
+
+
+hourlyForecastDecoder : Json.Decoder HourlyForecast
+hourlyForecastDecoder =
+  Json.object6
+    HourlyForecast
+    ("time" := Json.int)
+    ("summary" := Json.string)
+    ("icon" := Json.string)
+    ("temperature" := Json.float)
+    ("precipIntensity" := Json.float)
+    ("precipProbability" := Json.float)
+
+
+forecastDecoder : Json.Decoder Forecast
+forecastDecoder =
+  Json.object1 Forecast ("time" := Json.int)
+    `apply` ("summary" := Json.string)
+    `apply` ("icon" := Json.string)
+    `apply` ("precipIntensity" := Json.float)
+    `apply` ("precipProbability" := Json.float)
+    `apply` ("temperature" := Json.float)
+    `apply` ("windSpeed" := Json.float)
+    `apply` ("windBearing" := Json.float)
+    `apply` ("humidity" := Json.float)
+    `apply` ("visibility" := Json.float)
+    `apply` ("cloudCover" := Json.float)
+    `apply` ("pressure" := Json.float)
+    `apply` ("ozone" := Json.float)
+
+
+dailyForecastDecoder : Json.Decoder DailyForecast
+dailyForecastDecoder =
+  Json.object7
+    DailyForecast
+    ("time" := Json.int)
+    ("summary" := Json.string)
+    ("icon" := Json.string)
+    ("precipIntensity" := Json.float)
+    ("precipProbability" := Json.float)
+    ("temperatureMin" := Json.float)
+    ("temperatureMax" := Json.float)
+
+
+completeForecastDecoder : Json.Decoder (Maybe CompleteForecast)
+completeForecastDecoder =
+  Json.maybe <|
+    Json.object6
+      CompleteForecast
+      ("latitude" := Json.float)
+      ("longitude" := Json.float)
+      ("timezone" := Json.string)
+      ("currently" := forecastDecoder)
+      ("hourly" := (timespanForecastDecoder hourlyForecastDecoder))
+      ("daily" := (timespanForecastDecoder dailyForecastDecoder))
+
+
+apply : Json.Decoder (a -> b) -> Json.Decoder a -> Json.Decoder b
+apply func value =
+  Json.object2 (<|) func value
+
 -- MODEL
-
-type alias HourlyDataModel =
-  { time: Int,
-    summary: String,
-    icon: String,
-    precipIntensity: Float,
-    precipProbability: Float,
-    temperature: Float,
-    apparentTemperature: Float,
-    dewPoint: Float,
-    humidity: Float,
-    windSpeed: Float,
-    windBearing: Int,
-    visibility: Float,
-    cloudCover: Float,
-    pressure: Float,
-    ozone: Float
-  }
-
-type alias HourlyModel =
-  { summary: String,
-    icon: String,
-    data: List HourlyDataModel
-  }
-
 type alias Model =
-  { hourly: Maybe HourlyModel,
-    latitude: Float,
-    longitude: Float
+  { completeForecast: (Maybe CompleteForecast)
+    , latitude: Float
+    , longitude: Float
   }
 
 init : Float -> Float -> (Model, Cmd Msg)
 init latitude longitude =
-  ({  latitude = latitude
+  ({  completeForecast = Nothing
+    , latitude = latitude
     , longitude = longitude }
     , getWeather latitude longitude
   )
@@ -61,7 +153,7 @@ init latitude longitude =
 
 type Msg
   = Update
-  | FetchSucceed String
+  | FetchSucceed (Maybe CompleteForecast)
   | FetchFail Http.Error
 
 
@@ -71,21 +163,31 @@ update msg model =
     Update ->
       (model, getWeather model.latitude model.longitude)
 
-    FetchSucceed hourly ->
-      ({ model | hourly = hourly }, Cmd.none)
+    FetchSucceed completeForecast ->
+      ({ model | completeForecast = completeForecast }, Cmd.none)
 
     FetchFail _ ->
       (model, Cmd.none)
+
+-- HTTP
+
+
+getWeather : Float -> Float -> Cmd Msg
+getWeather latitude longitude =
+  let
+    url =
+      "https://api.forecast.io/forecast/4a726f371f08249dadae62caaacfdcd8/" ++ toString latitude ++ "," ++ toString longitude
+  in
+    Task.perform FetchFail FetchSucceed (Http.get completeForecastDecoder url)
 
 
 
 -- VIEW
 
-
 view : Model -> Html Msg
 view model =
   div []
-    [ h2 [] [text model.longitude.toString]
+    [ h2 [] [text (toString model.longitude)]
     , button [ onClick Update ] [ text "Update!" ]
     , br [] []
     ]
@@ -98,22 +200,3 @@ view model =
 subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.none
-
-
-
--- HTTP
-
-
-getWeather : Float -> Float -> Cmd Msg
-getWeather latitude longitude =
-  let
-    url =
-      "https://api.forecast.io/forecast/4a726f371f08249dadae62caaacfdcd8/" ++ latitude ++ "," ++ longitude
-  in
-    Task.perform FetchFail FetchSucceed (Http.get decodeWeather url)
-
-
-decodeWeather : Decoder HourlyModel
-decodeWeather =
-  decode HourlyModel
-    |> Json.Decode.Pipeline.required "hourly" HourlyModel
